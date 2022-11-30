aa279e0307f10815c62cd855e0c2d8d015520160
Anisha Palei <anishapalei073@gmail.com>
2022-11-30 18:16:56 +0530

    Created SongRepository.java

---
+++ src/main/java/com/niit/jdp/repositories/SongRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Author : Anisha Palei
+ * Date : 30-11-2022
+ * Created with : IntelliJ IDEA Community Edition
+ */
+
+package com.niit.jdp.repositories;
+
+import com.niit.jdp.model.Song;
+
+import java.util.List;
+
+public class SongRepository {
+
+
+}
