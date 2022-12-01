0f19174a9566c7547e23244937bac523d421e470
Anisha Palei <anishapalei073@gmail.com>
2022-12-01 12:37:13 +0530

    Wrote logic to display all the songs

--- src/main/java/com/niit/jdp/repository/SongRepository.java
+++ src/main/java/com/niit/jdp/repository/SongRepository.java
@@ -7,14 +7,47 @@
 package com.niit.jdp.repository;
 
 import com.niit.jdp.model.Song;
+import com.niit.jdp.service.DatabaseConnectionService;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SongRepository {
-
-    public List<Song> displaySongList()
+    Connection connection;
+  DatabaseConnectionService databaseService;
+    List<Song> finalSongList;
+    public SongRepository() throws SQLException, ClassNotFoundException {
+        databaseService = new DatabaseConnectionService();
+        connection = databaseService.getConnectionToDatabase();
+        finalSongList=new ArrayList<>();
+    }
+    public List<Song> displaySongs()
     {
-        return null;
+        String query="Select * from jukebox.song;";
+        try (Statement statement = connection.createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery(query);
+            while(resultSet.next())
+            {
+                int songId = resultSet.getInt("song_id");
+                String songName = resultSet.getString("song_name");
+                String songDuration = resultSet.getString("duration");
+                String genreType = resultSet.getString("genre_type");
+                String albumName = resultSet.getString("album_name");
+                String artistName = resultSet.getString("artist_name");
+                Song song=new Song(songId,songName,songDuration,genreType,artistName,albumName);
+                finalSongList.add(song);
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return finalSongList;
+
     }
     public void searchSongBySongName()
     {
