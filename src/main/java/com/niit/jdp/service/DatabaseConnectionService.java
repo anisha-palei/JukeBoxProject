355502be6f1243557f60aef4d455b7815517147f
Anisha Palei <anishapalei073@gmail.com>
2022-12-01 12:33:26 +0530

    Wrote logic to connect to database

--- src/main/java/com/niit/jdp/service/DatabaseConnectionService.java
+++ src/main/java/com/niit/jdp/service/DatabaseConnectionService.java
@@ -6,5 +6,26 @@
 
 package com.niit.jdp.service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnectionService {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/sales";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+    Connection connection;
+
+    public DatabaseConnectionService() {
+        connection=null;
+    }
+    private void connect() throws ClassNotFoundException, SQLException {
+        connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+
+    public Connection getConnectionToDatabase() throws SQLException, ClassNotFoundException {
+        connect();
+        return connection;
+    }
 }
