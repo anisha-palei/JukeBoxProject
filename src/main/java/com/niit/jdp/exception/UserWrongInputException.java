6297564b5d15ddecf544458558371d19515ea971
Anisha Palei <anishapalei073@gmail.com>
2022-12-03 10:00:04 +0530

    Created custom exception

---
+++ src/main/java/com/niit/jdp/exception/UserWrongInputException.java
@@ -0,0 +1,14 @@
+/*
+ * Author : Anisha Palei
+ * Date : 01-12-2022
+ * Created with : IntelliJ IDEA Community Edition
+ */
+
+package com.niit.jdp.exception;
+
+public class UserWrongInputException extends Exception{
+    public UserWrongInputException(String message)
+    {
+        super(message);
+    }
+}
