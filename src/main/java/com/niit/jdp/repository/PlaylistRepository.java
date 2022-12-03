10bb9bc31ad2b1a8f6cd43c8171fc3bb95e57691
Anisha Palei <anishapalei073@gmail.com>
2022-12-03 17:02:44 +0530

    created createPlaylist method

--- src/main/java/com/niit/jdp/repository/PlaylistRepository.java
+++ src/main/java/com/niit/jdp/repository/PlaylistRepository.java
@@ -6,18 +6,49 @@
 
 package com.niit.jdp.repository;
 
+import com.niit.jdp.model.Playlist;
+import com.niit.jdp.model.Song;
+import com.niit.jdp.service.DatabaseConnectionService;
+import com.niit.jdp.service.MusicPlayerService;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PlaylistRepository {
+    Connection connection;
+    DatabaseConnectionService databaseConnectionService;
+    List<Song> finalSongList;
+    MusicPlayerService musicPlayerService;
 
-    public void createPlaylist()
-    {
-
+    public PlaylistRepository() throws SQLException, ClassNotFoundException {
+        databaseConnectionService=new DatabaseConnectionService();
+        connection = databaseConnectionService.getConnectionToDatabase();
+        finalSongList = new ArrayList<>();
+        musicPlayerService=new MusicPlayerService();
     }
-    public void playPlaylist()
-    {}
-    public void addSongToPlayList()
-    {}
-    public void displayContentPlayList()
-    {}
-    public void displayAllSongs()
-    {}
+
+    public Playlist createPlaylist(String playlistName)
+    {
+        Playlist playlist = new Playlist();
+        String insertQuery = "insert into jukebox.playlist values (?);";
+        try (PreparedStatement statement = connection.prepareStatement(insertQuery, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, playlistName);
+            int result = statement.executeUpdate();
+            if (result > 0) {
+                ResultSet keys = statement.getGeneratedKeys();
+                if (keys.next()) {
+                    playlist.setPlaylistId(keys.getInt(1));
+                    playlist.setPlaylistName(playlistName);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return playlist;
+    }
+
 }
