170daca587edc0e27080dcf0c0f472c504295589
Anisha Palei <anishapalei073@gmail.com>
2022-12-03 10:02:18 +0530

    wrote logic to play and stop music

--- src/main/java/com/niit/jdp/service/MusicPlayerService.java
+++ src/main/java/com/niit/jdp/service/MusicPlayerService.java
@@ -6,5 +6,43 @@
 
 package com.niit.jdp.service;
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class MusicPlayerService {
+    Scanner scanner=new Scanner(System.in);
+    public void play(String songPath) {
+
+        File songFile = new File(songPath);
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(songFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            System.out.println("Enter 1. Start\t\n 2.stop \t\n3.Back to Main Menu");
+            int choice=0;
+
+            do {
+                choice=scanner.nextInt();
+                switch (choice) {
+                    case 1: {
+                        clip.start();
+                        break;
+                    }
+                    case 2: {
+                        clip.stop();
+                        break;
+                    }
+                    default: {
+                        System.out.println("");
+                    }
+
+                }
+            }while (choice==1 || choice==2);
+
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException exception) {
+            exception.printStackTrace();
+        }
+    }
 }
