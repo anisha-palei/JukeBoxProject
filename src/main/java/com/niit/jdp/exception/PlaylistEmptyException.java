3bfb5c3dd5ccc95c94ae81610750847fedcfd37a
Anisha Palei <anishapalei073@gmail.com>
2022-12-04 00:50:34 +0530

    created custom exception

---
+++ src/main/java/com/niit/jdp/exception/PlaylistEmptyException.java
@@ -0,0 +1,14 @@
+/*
+ * Author : Anisha Palei
+ * Date : 03-12-2022
+ * Created with : IntelliJ IDEA Community Edition
+ */
+
+package com.niit.jdp.exception;
+
+public class PlaylistEmptyException extends Exception {
+
+    public PlaylistEmptyException(String message) {
+        super(message);
+    }
+}
